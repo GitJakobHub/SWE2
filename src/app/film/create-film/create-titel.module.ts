8746fa07acf996a4deea9a9c4d8283d4b4af049c
GitJakobHub <scja1070@hs-karlsruhe.de>
2021-01-24 22:45:21 +0100

    titel fast fertig

--- src/app/film/create-film/create-titel.module.ts
+++ src/app/film/create-film/create-titel.module.ts
@@ -17,12 +17,13 @@
 
 import { CommonModule } from '@angular/common';
 import { CreateTitelComponent } from './create-titel.component';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [CreateTitelComponent],
     exports: [CreateTitelComponent],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule, MatInputModule],
 })
 export class CreateTitelModule {}
